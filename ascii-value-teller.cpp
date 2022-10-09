b6b8ffd02ffbe21e08e03b9996e6ecbf864044a4
Muhammad Zohaib Arshad <muhammadzohaibarshad18@gmail.com>
2022-10-09 15:10:14 +0500

    my first commit for hacktoberfest-2022

---
+++ ascii-value-teller.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char x;
+	cout << "Enter a character: ";
+	cin >> x;
+	cout << "The ascii value of x is: " << x << " is " << int(x);
+	return 0;
+}
